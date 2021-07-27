72df3407dd15e4762801d205e14caa04ff188796
gschnabel <40870991+gschnabel@users.noreply.github.com>
2021-07-27 10:15:22 +0200

    switch to indexing via boolean vector

--- R/inference.R
+++ R/inference.R
@@ -1,7 +1,6 @@
-
 gls <- function(map, zprior, U, obs, zref=zprior) {
 
-  obsmask <- which(!is.na(obs))
+  obsmask <- !is.na(obs)
 
   # prepare the Taylor expansion
   zref[obsmask] <- 0
@@ -11,15 +10,17 @@ gls <- function(map, zprior, U, obs, zref=zprior) {
 
   # prepare the statistical model description
   # and auxiliary quantities
-  invU <- solve(U, sparse=TRUE)
+  adjustable <- diag(U) > 0
+  Ured <- U[adjustable, adjustable]
+  invU <- solve(Ured, sparse=TRUE)
 
   v <- zprior
   v[obsmask] <- obs[obsmask]
   vref <- zref
   vref[obsmask] <- yref[obsmask]
 
-  Sred <- S[obsmask,-obsmask]
-  Sred1 <- S[,-obsmask]
+  Sred <- S[obsmask,!obsmask]
+  Sred1 <- S[,!obsmask]
   invPostU_red <- forceSymmetric(t(Sred1) %*% invU %*% Sred1)
 
   s1 <- solve(U, v - vref)
@@ -27,10 +28,9 @@ gls <- function(map, zprior, U, obs, zref=zprior) {
   s3 <- solve(invPostU_red, s2)
 
   zpost <- rep(0, length(zprior))
-  zpost[-obsmask] <- zref[-obsmask] + s3
+  zpost[!obsmask] <- zref[!obsmask] + s3
 
   # calculate posterior of experimental values
-  zpost[obsmask] <- zprior[obsmask] - Sred %*% zpost[-obsmask]
+  zpost[obsmask] <- obs[obsmask] - Sred %*% zpost[!obsmask]
   zpost
 }
-

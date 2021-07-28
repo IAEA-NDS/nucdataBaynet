0b5fb67957d77de6806a9075d91ab336ddccc0e0
gschnabel <40870991+gschnabel@users.noreply.github.com>
2021-07-28 11:55:24 +0200

    add LM algorithm and damp parameter in GLS method

--- R/inference.R
+++ R/inference.R
@@ -1,4 +1,4 @@
-gls <- function(map, zprior, U, obs, zref=zprior, ret.list=FALSE) {
+gls <- function(map, zprior, U, obs, zref=zprior, damp=0, ret.list=FALSE) {
 
   obsmask <- !is.na(obs)
 
@@ -12,7 +12,9 @@ gls <- function(map, zprior, U, obs, zref=zprior, ret.list=FALSE) {
   # and auxiliary quantities
   adjustable <- diag(U) > 0
   isindep <- !obsmask & adjustable
+
   stopifnot(all(diag(U)[obsmask] > 0))
+  stopifnot(all(zref[!adjustable] == zprior[!adjustable]))
 
   Ured <- U[adjustable,adjustable]
 
@@ -27,6 +29,8 @@ gls <- function(map, zprior, U, obs, zref=zprior, ret.list=FALSE) {
   Sred <- S[obsmask, isindep]
   Sred1 <- S[adjustable, isindep]
   invPostU_red <- forceSymmetric(crossprod(Sred1, solve(Ured, Sred1, sparse=TRUE)))
+  # if run in the loop of Levenberg-Marquart optimization
+  diag(invPostU_red) <- diag(invPostU_red) + damp
 
   s1 <- solve(Ured, v - vref)
   s2 <- t(Sred1) %*% s1
@@ -45,4 +49,82 @@ gls <- function(map, zprior, U, obs, zref=zprior, ret.list=FALSE) {
   } else {
     return(zpost)
   }
+}
+
+
+LMalgo <- function(map, zprior, U, obs, zref=zprior, ret.invcov=FALSE) {
+
+  # LM parameters
+  reltol <- 1e-5
+  maxcount <- 200
+
+  adjustable <- diag(U) > 0
+  observed <- !is.na(obs)
+  isindep <- adjustable & !observed
+  adjobs <- observed[adjustable]
+  tau <- 1e-3
+  Ured <- U[adjustable, adjustable]
+
+  # determine the apriori scale for the damping term
+  S <- map$jacobian(zref, with.id=TRUE)
+  S <- S[adjustable, isindep]
+  invUred_post <- crossprod(S, solve(Ured, S))
+  lambda <- tau * max(invUred_post)
+  remove(S, invUred_post)
+
+  # run the LM optimization loop
+  yref <- map$propagate(zref)
+  zref[observed] <- obs[observed] - yref[observed]
+  last_d <- zref[adjustable] - zprior[adjustable]
+  init_fex <- as.vector(crossprod(last_d, solve(Ured, last_d)))
+  last_fapx <- last_fex <- init_fex
+  cnt <- 0
+  last_reject <- TRUE
+  rel_gain <- Inf
+  while (cnt < maxcount && !(last_reject == FALSE && relgain <= reltol)) {
+    cnt <- cnt + 1
+    zprop <- gls(map, zprior, U, obs, zref=zref, damp=lambda)
+    # calculate improvement according to linear approximation
+    dapx <- zprop[adjustable] - zprior[adjustable]
+    fapx <- as.vector(crossprod(dapx, solve(Ured, dapx)))
+    # calculate improvement according to exact nonlinear map
+    excess <- map$propagate(zprop)[observed] - obs[observed]
+    dex <- dapx
+    dex[adjobs] <- dex[adjobs] - excess
+    fex <- as.vector(crossprod(dex, solve(Ured, dex)))
+    # calculate the gain
+    gain <- (last_fex - fex) / (last_fapx - fapx)
+    relgain <- (last_fex - fex) / fex
+    # adjust damping
+    if (gain < 0.25) {
+      lambda <- lambda * 2
+    } else if (gain > 0.75) {
+      lambda <- lambda / 3
+    }
+    # accept proposal if better (smaller) than current one
+    if (fex - last_fex < 0) {
+      zref <- zprop
+      last_fapx <- fapx
+      last_fex <- fex
+      last_reject <- FALSE
+    } else {
+      last_reject <- TRUE
+    }
+  }
+
+  res <- list(
+    zpost = zref,
+    init_val = init_fex,
+    final_val = last_fex
+  )
+  # determine the a posteriori covariance matrix
+  if (ret.invcov) {
+    S <- map$jacobian(zref, with.id=TRUE)
+    S <- S[adjustable, isindep]
+    invU <- crossprod(S, solve(Ured, S))
+    mask <- isindep
+    res[["invcov"]] <- invU
+    res[["covmask"]] <- mask
+  }
+  return(res)
 }
---
+++ tests/testthat/test-LMalgo.R
@@ -0,0 +1,96 @@
+truexs_dt <- data.table(node="true",
+                        energy = 1:5,
+                        data = 0,
+                        obs = exp(1:5))
+
+normerr_dt <- data.table(node="normerr",
+                         energy = NA,
+                         data = 0,
+                         unc = 0.2,
+                         obs = NA)
+
+expxs_dt <- data.table(node="exp",
+                       energy = truexs_dt$energy,
+                       data = 0,
+                       unc = 0.05,
+                       obs = exp(1:5)*(1 + rnorm(1, sd=normerr_dt$unc[1])) +
+                                         rnorm(5, sd=0.05))
+
+modxs_dt <- data.table(node="mod",
+                       energy = c(1,5),
+                       data = 0,
+                       unc = 1e8,
+                       obs = NA)
+
+modxs2_dt <- data.table(node="mod2",
+                       energy = 1:5,
+                       data = 0,
+                       unc = 0,
+                       obs = NA)
+
+
+dt <- rbindlist(list(modxs_dt, modxs2_dt, normerr_dt, expxs_dt), use.names=TRUE)
+dt[, IDX := seq_len(.N)]
+
+
+linmod_params <- list(
+  mapname = "linmod_map",
+  src_idx = dt[node=="mod",IDX],
+  tar_idx = dt[node=="mod2",IDX],
+  src_x = dt[node=="mod",energy],
+  tar_x = dt[node=="mod2",energy]
+)
+
+
+nonlinear_params <- list(
+  mapname = "nonlinear_map",
+  src_idx = dt[node=="mod2",IDX],
+  tar_idx = dt[node=="exp",IDX],
+  funname = "exp"
+)
+
+
+normerr_params <- normerr_map <- list(
+  mapname = "normerr_map",
+  src_idx = dt[node=="normerr",IDX],
+  tar_idx = dt[node=="exp",IDX],
+  src_feat = 1,
+  tar_feat = dt[node=="exp", rep(1, .N)]
+)
+
+
+relnormerr_params <- list(
+  mapname = "product_map",
+  maps = list(nonlinear_params, normerr_params)
+)
+
+compound_params <- list(
+  mapname = "compound_map",
+  maps = list(linmod_params, nonlinear_params, relnormerr_params)
+)
+
+comp_map <- create_compound_map()
+comp_map$setup(compound_params)
+
+
+test_that("compound map jacobian corresponds to numeric jacobian", {
+  myinp <- runif(nrow(dt))
+  res1 <- jacobian(comp_map$propagate, myinp, with.id=TRUE)
+  res2 <- as.matrix(comp_map$jacobian(myinp, with.id=TRUE))
+  dimnames(res1) <- NULL
+  dimnames(res2) <- NULL
+  expect_equal(res1, res2)
+})
+
+
+test_that("LM algorithm converges to same solution from different starting points", {
+  zprior <- dt$data
+  U <- Diagonal(x = dt$unc^2)
+  zstart1 <- zstart2 <- zprior
+  sel <- dt[node %in% c("mod", "normerr"),IDX]
+  zstart1[sel] <- c(4, 2, 1)
+  zstart2[sel] <- c(1, 3, 3)
+  curres1 <- LMalgo(comp_map, zprior, U, dt$obs, zref=zstart1, ret.invcov=FALSE)
+  curres2 <- LMalgo(comp_map, zprior, U, dt$obs, zref=zstart2, ret.invcov=FALSE)
+  expect_equal(curres1$final_val, curres2$final_val)
+})

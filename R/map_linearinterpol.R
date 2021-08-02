522143f0c86c8a28050d5bfacf80ba9597895bfb
gschnabel <40870991+gschnabel@users.noreply.github.com>
2021-08-02 12:57:22 +0200

    improve jacobian of linearinterpol_map & add public getter/setter functions

--- R/map_linearinterpol.R
+++ R/map_linearinterpol.R
@@ -38,14 +38,6 @@ create_linearinterpol_map <- function() {
     return(TRUE)
   }
 
-  get_src_idx <- function() {
-    return(map$src_idx)
-  }
-
-  get_tar_idx <- function() {
-    return(map$tar_idx)
-  }
-
   propagate <- function(x, with.id=TRUE) {
     S <- jacobian(x, with.id)
     return(as.vector(S %*% x))
@@ -53,7 +45,7 @@ create_linearinterpol_map <- function() {
 
   jacobian <- function(x, with.id=TRUE) {
     if (is.null(S) || is_with_id != with.id) {
-      idx1 <- findInterval(map$tar_x, map$src_x, all.inside=TRUE)
+      idx1 <- findInterval(map$tar_x, map$src_x, rightmost.closed=TRUE)
       idx2 <- idx1 + 1
       gidx1 <- map$src_idx[idx1]
       gidx2 <- map$src_idx[idx2]
@@ -74,6 +66,34 @@ create_linearinterpol_map <- function() {
     return(S)
   }
 
+
+  # additional functions not part of standard interface
+
+  get_src_idx <- function() {
+    return(map$src_idx)
+  }
+
+  get_tar_idx <- function() {
+    return(map$tar_idx)
+  }
+
+  get_src_x <- function() {
+    return(map$src_x)
+  }
+
+  get_tar_x <- function() {
+    return(map$tar_x)
+  }
+
+  set_tar_x <- function(tar_x) {
+    min_src_x <- map$src_x[1]
+    max_src_x <- tail(map$src_x, n=1)
+    stopifnot(all(tar_x >= min_src_x) && all(tar_x <= max_src_x))
+    S <<- NULL
+    map[["tar_x"]] <<- tar_x
+  }
+
+
   list(
     setup = setup,
     getType = getType,
@@ -83,6 +103,12 @@ create_linearinterpol_map <- function() {
     get_src_idx = get_src_idx,
     get_tar_idx = get_tar_idx,
     propagate = propagate,
-    jacobian = jacobian
+    jacobian = jacobian,
+    # additional functions not part of standard interface
+    get_src_idx = get_src_idx,
+    get_tar_idx = get_tar_idx,
+    get_src_x = get_src_x,
+    get_tar_x = get_tar_x,
+    set_tar_x = set_tar_x
   )
 }

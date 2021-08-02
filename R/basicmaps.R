ac0cd030bfab4e251383eaaeb4d63403dd468f59
gschnabel <40870991+gschnabel@users.noreply.github.com>
2021-08-02 19:56:43 +0200

    add basic convolution maps and unit tests

---
+++ R/basicmaps.R
@@ -0,0 +1,60 @@
+get_convolute_rect_matrix <- function(src_x, tar_x, winsize) {
+  stopifnot(!is.unsorted(src_x))
+  tar_x_min <- tar_x - winsize/2
+  tar_x_max <- tar_x + winsize/2
+  tar_idx <- seq_along(tar_x)
+  min_idx <- findInterval(tar_x_min, src_x) + 1
+  max_idx <- findInterval(tar_x_max, src_x)
+  # for all segments of src_x completely inside apply simpson integration rule
+  xdiff <- src_x[-1] - head(src_x, n=-1)
+  coeff <- xdiff/2
+  imi <- sequence(max_idx-min_idx, tar_idx, by=0L)
+  jmi <- sequence(max_idx-min_idx, min_idx, by=1)
+  x <- (src_x[jmi+1] - src_x[jmi])/2
+  # edge correction lower bound
+  xmeshdiff <- src_x[min_idx] - src_x[min_idx-1]
+  xrealdiff <- src_x[min_idx] - tar_x_min
+  ilo <- tar_idx
+  jlo <- min_idx
+  # coefficients for linear interpolation to get funval at tar_x_min
+  c1lo <- (src_x[min_idx] - tar_x_min) / xmeshdiff
+  c2lo <- (tar_x_min - src_x[min_idx-1]) / xmeshdiff
+  # multiplication to get integral
+  c1lo <- c1lo * xrealdiff/2
+  c2lo <- (1+c2lo) * xrealdiff/2
+  # edge correction upper bound
+  xmeshdiff <- src_x[max_idx+1] - src_x[max_idx]
+  xrealdiff <- tar_x_max - src_x[max_idx]
+  ihi <- tar_idx
+  jhi <- max_idx
+  # coefficients for linear interpolation to get funval at tar_x_max
+  c1hi <- (src_x[max_idx+1] - tar_x_max) / xmeshdiff
+  c2hi <- (tar_x_max - src_x[max_idx]) / xmeshdiff
+  # multiplication to get integral
+  c1hi <- (1+c1hi) * xrealdiff/2
+  c2hi <- c2hi * xrealdiff/2
+  # put everything together
+  convmat <- sparseMatrix(i = c(rep(imi, 2), rep(ilo, 2), rep(ihi, 2)),
+               j = c(jmi, jmi+1, jlo-1, jlo, jhi, jhi+1),
+               x = c(rep(x, 2), c1lo, c2lo, c1hi, c2hi),
+               dims=c(length(tar_x), length(src_x)))
+  # calculate derivative wrt window if requested
+  return(convmat)
+}
+
+
+get_convolute_rect_derivative <- function(src_x, src_y, tar_x, winsize) {
+  stopifnot(!is.unsorted(src_x))
+  tar_x_min <- tar_x - winsize/2
+  tar_x_max <- tar_x + winsize/2
+  tar_idx <- seq_along(tar_x)
+  min_idx <- findInterval(tar_x_min, src_x, rightmost.closed=TRUE)
+  max_idx <- findInterval(tar_x_max, src_x, rightmost.closed=TRUE)
+  # get the function value
+  f1 <- (src_y[min_idx] * (src_x[min_idx+1] - tar_x_min) +
+           src_y[min_idx+1] * (tar_x_min - src_x[min_idx])) / (src_x[min_idx+1] - src_x[min_idx])
+  f2 <- (src_y[max_idx] * (src_x[max_idx+1] - tar_x_max) +
+         src_y[max_idx+1] * (tar_x_max - src_x[max_idx])) / (src_x[max_idx+1] - src_x[max_idx])
+  deriv <- (f1 + f2) / 2
+  return(deriv)
+}
---
+++ tests/testthat/test-basicmaps.R
@@ -0,0 +1,71 @@
+test_that("get convolute_rect_matrix yields correct result", {
+  set.seed(27)
+  src_x <- cumsum(runif(100, min=1, max=5))
+  tar_x <- 50
+  winsize <- 30
+  vals <- seq_along(src_x)
+  S <- get_convolute_rect_matrix(src_x, tar_x, 30)
+  aug_src_x <- sort(c(src_x, tar_x-winsize/2, tar_x+winsize/2))
+  aug_vals <- approx(src_x, vals, xout=aug_src_x)$y
+  xdiff <- aug_src_x[-1] - head(aug_src_x, n=-1)
+  c1 <- head(aug_vals, n=-1) * xdiff/2
+  c2 <- aug_vals[-1] * xdiff/2
+  area <- c1 + c2
+  cumarea <- c(0, cumsum(area))
+  idcs <- findInterval(tar_x+c(-winsize,winsize)/2, aug_src_x)
+  expres <- diff(cumarea[idcs])
+  res <- as.vector(S %*% vals)
+  expect_equal(res, expres)
+  # also check with augmented grid
+  S <- get_convolute_rect_matrix(aug_src_x, tar_x, 30)
+  res <- as.vector(S %*% aug_vals)
+  expect_equal(res, expres)
+})
+
+
+test_that("get_convolute_rect_matrix yields correct result if no segment completely inside", {
+  src_x <- c(1, 55, 100)
+  tar_x <- 50
+  winsize <- 30
+  vals <- seq_along(src_x)
+  S <- get_convolute_rect_matrix(src_x, tar_x, 30)
+  aug_src_x <- sort(c(src_x, tar_x-winsize/2, tar_x+winsize/2))
+  aug_vals <- approx(src_x, vals, xout=aug_src_x)$y
+  xdiff <- aug_src_x[-1] - head(aug_src_x, n=-1)
+  c1 <- head(aug_vals, n=-1) * xdiff/2
+  c2 <- aug_vals[-1] * xdiff/2
+  area <- c1 + c2
+  cumarea <- c(0, cumsum(area))
+  idcs <- findInterval(tar_x+c(-winsize,winsize)/2, aug_src_x)
+  expres <- diff(cumarea[idcs])
+  res <- as.vector(S %*% vals)
+  expect_equal(res, expres)
+})
+
+
+test_that("compound convolution map for several points equals sum of individual convolution maps", {
+  set.seed(27)
+  src_x <- cumsum(runif(30, min=3, max=5))
+  tar_x <- c(30, 35, 40)
+  winsize <- 20
+  Scomp <- get_convolute_rect_matrix(src_x, tar_x, winsize)
+  Sadd <- NULL
+  for (cur_tar_x in tar_x) {
+    Sadd <- rbind(Sadd, get_convolute_rect_matrix(src_x, cur_tar_x, winsize))
+  }
+  expect_equal(Scomp, Sadd)
+})
+
+
+test_that("convolute_rect_derivative conincides with numerical jacobian", {
+  src_x <- c(1, 55, 100)
+  tar_x <- 50
+  winsize <- 30
+  vals <- seq_along(src_x)
+  fun <- function(x) {
+    as.vector(get_convolute_rect_matrix(src_x, tar_x, winsize=x) %*% vals)
+  }
+  expres <- as.vector(jacobian(fun, 30))
+  res <- get_convolute_rect_derivative(src_x, vals, tar_x, 30)
+  expect_equal(res, expres)
+})

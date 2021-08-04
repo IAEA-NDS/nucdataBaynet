00a72c3b22a844c0cc20bb3d5e7de2564f8c2647
gschnabel <40870991+gschnabel@users.noreply.github.com>
2021-08-04 14:40:35 +0200

    introduce zero_outside parameter to linearinterpol_map

--- R/map_linearinterpol.R
+++ R/map_linearinterpol.R
@@ -2,24 +2,24 @@ create_linearinterpol_map <- function() {
 
   map <- NULL
   S <- NULL
-  is_with_id <- FALSE
 
   setup <- function(params) {
     stopifnot(params$maptype == getType())
-    stopifnot(!is.unsorted(params$src_x))
-    stopifnot(min(params$src_x) <= min(params$tar_x))
-    stopifnot(max(params$src_x) >= max(params$tar_x))
     stopifnot(all(c('maptype','src_idx','tar_idx','src_x','tar_x') %in% names(params)))
-    S <<- NULL
+    src_ord <- order(params$src_x)
     map <<- list(
       maptype = params$maptype,
       mapname = params$mapname,
       description = params$description,
-      src_idx = params$src_idx,
+      src_idx = params$src_idx[src_ord],
       tar_idx = params$tar_idx,
-      src_x = params$src_x,
-      tar_x = params$tar_x
+      src_x = params$src_x[src_ord],
+      tar_x = params$tar_x,
+      zero_outside = params$zero_outside
     )
+    if (!isTRUE(map$zero_outside)) {
+      check_mesh(map$tar_x)
+    }
   }
 
   getType <- function() {
@@ -39,33 +39,65 @@ create_linearinterpol_map <- function() {
   }
 
   propagate <- function(x, with.id=TRUE) {
-    S <- jacobian(x, with.id)
-    return(as.vector(S %*% x))
+    if (is.null(S)) {
+      update_jacobian(length(x))
+    }
+    if (isTRUE(with.id)) {
+      return(x + as.vector(S %*% x))
+    } else {
+      return(as.vector(S %*% x))
+    }
   }
 
   jacobian <- function(x, with.id=TRUE) {
-    if (is.null(S) || is_with_id != with.id) {
-      idx1 <- findInterval(map$tar_x, map$src_x, rightmost.closed=TRUE)
-      idx2 <- idx1 + 1
-      gidx1 <- map$src_idx[idx1]
-      gidx2 <- map$src_idx[idx2]
-      x1 <- map$src_x[idx1]
-      x2 <- map$src_x[idx2]
-      xp <- map$tar_x
-      delta <- x2 - x1
-      i <- rep(map$tar_idx, 2)
-      j <- c(gidx1, gidx2)
-      coeff <- c((x2-xp)/delta, (xp-x1)/delta)
-      S <<- sparseMatrix(i = i, j = j, x = coeff,
-                        dims = rep(length(x),2))
-      if (with.id) {
-        diag(S) <<- diag(S) + 1
-      }
-      is_with_id <<- with.id
+    if (is.null(S)) {
+      update_jacobian(length(x))
     }
-    return(S)
+    if (isTRUE(with.id)) {
+      return(S + Diagonal(n=length(x), x=1))
+    } else {
+      return(S)
+    }
   }
 
+  # internal functions
+
+  check_mesh <- function(tar_x) {
+    if (min(tar_x) < map$src_x[1]) {
+      stop(paste0("some target x are smaller than the lowest x of the mesh",
+                  " (e.g., x = ", min(tar_x), ")"))
+    }
+    if (max(tar_x) > tail(map$src_x, n=1)) {
+      stop(paste0("some target x are larger than the largest x of the mesh",
+                  " (e.g., x = ", max(tar_x), ")"))
+    }
+  }
+
+
+  update_jacobian <- function(dim) {
+    idx1 <- findInterval(map$tar_x, map$src_x, rightmost.closed=TRUE)
+    idx2 <- idx1 + 1
+    if (isTRUE(map$zero_outside)) {
+      sel <- idx1 >= 1 & idx2 < length(map$tar_x)
+      idx1 <- idx1[sel]
+      idx2 <- idx2[sel]
+      tar_idx <- map$tar_idx[sel]
+      xp <- map$tar_x[sel]
+    } else {
+      tar_idx <- map$tar_idx
+      xp <- map$tar_x
+    }
+    gidx1 <- map$src_idx[idx1]
+    gidx2 <- map$src_idx[idx2]
+    x1 <- map$src_x[idx1]
+    x2 <- map$src_x[idx2]
+    delta <- x2 - x1
+    i <- rep(tar_idx, 2)
+    j <- c(gidx1, gidx2)
+    coeff <- c((x2-xp)/delta, (xp-x1)/delta)
+    S <<- sparseMatrix(i = i, j = j, x = coeff,
+                       dims = rep(dim,2))
+  }
 
   # additional functions not part of standard interface
 
@@ -86,13 +118,12 @@ create_linearinterpol_map <- function() {
   }
 
   set_tar_x <- function(tar_x) {
-    min_src_x <- map$src_x[1]
-    max_src_x <- tail(map$src_x, n=1)
-    stopifnot(all(tar_x >= min_src_x) && all(tar_x <= max_src_x))
-    S <<- NULL
+    if (!isTRUE(map$zero_outside)) {
+      check_mesh(tar_x)
+    }
     map[["tar_x"]] <<- tar_x
+    S <<- NULL
   }
-
 
   list(
     setup = setup,

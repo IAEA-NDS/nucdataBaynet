1d27c9222874ad8d72642bff8b88bf103b84d589
gschnabel <40870991+gschnabel@users.noreply.github.com>
2021-09-09 20:19:08 +0200

    start using Roxygen

---
+++ R/nucdataBaynet-package.R
@@ -0,0 +1,9 @@
+#' nucdataBaynet - Nuclear data evaluation with Bayesian networks
+#'
+#' Bayesian networks with multivariate priors on variables and
+#' linear and non-linear relationships between variables.
+#'
+#' @docType package
+#' @name nucdataBaynet
+#' @import data.table Matrix numDeriv
+NULL
